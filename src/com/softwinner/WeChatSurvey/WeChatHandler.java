ed0bd0a4b9d78a4ef06e77b98ec42afb08ade681
Heweihong <357710565@qq.com>
2014-10-27 19:44:08 +0800

    增加.project .classpath文件
    Signed-off-by: Heweihong <357710565@qq.com>

--- src/com/softwinner/WeChatSurvey/WeChatHandler.java
+++ src/com/softwinner/WeChatSurvey/WeChatHandler.java
@@ -1,4 +1,4 @@
-package com.softwinner.MicroMSGSurvey;
+package com.softwinner.WeChatSurvey;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/com/softwinner/WeChatSurvey/log.java
+++ src/com/softwinner/WeChatSurvey/log.java
@@ -1,4 +1,4 @@
-package com.softwinner.MicroMSGSurvey;
+package com.softwinner.WeChatSurvey;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
